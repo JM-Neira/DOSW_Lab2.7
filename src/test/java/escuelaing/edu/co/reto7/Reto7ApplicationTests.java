b73c663facf0e325ede12f364305d25c9e02f7a1
Juan Neira <juan.neira-z@mail.escuelaing.edu.co>
2026-02-12 17:18:12 -0500

    proyecto inicializado con spring initializr

---
+++ src/test/java/escuelaing/edu/co/reto7/Reto7ApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.reto7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
